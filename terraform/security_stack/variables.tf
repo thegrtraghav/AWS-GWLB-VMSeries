44cbfc431779cd522bdaaed65954debadf84693d
thegrtraghav <thegrtraghav@gmail.com>
2026-03-16 17:44:48 +0530

    Update variables.tf

--- terraform/security_stack/variables.tf
+++ terraform/security_stack/variables.tf
@@ -38,9 +38,10 @@ variable "transit_gw_id" {
   type = string
 }
 
-variable "public_key" {
-  description = "Public key string for AWS SSH Key Pair"
-  type = string
+variable "ssh_key_name" {
+  description = "Name of the SSH key pair existing in AWS key pairs and used to authenticate to VM-Series or test boxes"
+  type        = string
+  default     = ""
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
